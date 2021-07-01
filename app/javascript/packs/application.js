de43a2d01b27749d29cf28404e5fffde0ed9a2b6
xiangyuzi <ayuko.weiji@gmail.com>
2021-07-01 19:40:04 +0900

    AjaxAppの準備

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("../memo")  //..は1つ上の階層（ディレクトリの位置）を意味する
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
