813640eea256c31345f07f070a93384b1ac0ae4b
xiangyuzi <ayuko.weiji@gmail.com>
2021-07-02 15:46:17 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,17 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;  //レスポンスの中から投稿されたメモ情報を抽出、変数itemに格納。
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時:${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;  //関数buildHTMLの返り値にhtmlを指定。
+};
+
 function post (){
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
@@ -8,6 +22,16 @@ function post (){
     XHR.open("POST", "/posts", true);  //非同期で投稿したメモをdbに保存するのでHTTPメソッドはPOST。（メソッド、パス、非同期のON/OFF）
     XHR.responseType = "json";  //どういう形式でデータをレスポンスで渡すかの設定
     XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;  //処理を一端中止するよう指示。エラーが出たらこれ以降の処理は行われない。
+      };
+      const list = document.getElementById("list")
+      const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
   });
 };
 
